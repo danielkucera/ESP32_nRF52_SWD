652599004c233ba9e8202137694cf296b9f3a7ec
Daniel Kucera <daniel.kucera@gmail.com>
2023-01-23 23:42:02 +0100

    flash meter with button

--- ESP32_SWD_WIFI/ESP32_SWD_WIFI.ino
+++ ESP32_SWD_WIFI/ESP32_SWD_WIFI.ino
@@ -27,11 +27,45 @@ void setup()
 #endif
 }
 
+int flash_firmware(){
+  uint32_t temp = swd_init();
+  nrf_abort_all();
+  
+  if (temp != 0x2ba01477){
+    return 1;
+  }
+
+  for (int addr=0; addr < 50000; addr+=4096){
+    if (erase_page(0x8000 + addr)){
+      return 1;
+    }
+  }
+
+  String filename = String("/gas_meter.img");
+
+  return flash_file(0x8000, filename);
+}
+
+#define FLASH_BUTTON 0
+
 void loop()
 {
 #ifdef ENABLE_OTA
   ArduinoOTA.handle();
 #endif
+
+  if(digitalRead(FLASH_BUTTON)==0){
+    digitalWrite(LED, 1);
+
+    if (flash_firmware()){
+      Serial.println("Flash failed");
+    } else {
+      Serial.println("Flash OK!");
+      digitalWrite(LED, 0);
+    }
+
+  }
+
   if (get_glitcher())
   {
     do_glitcher();
